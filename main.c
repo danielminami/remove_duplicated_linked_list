a8a3074fe66ce7241e27f49deed248b62e7fc39d
Daniel <danielminami@hotmail.com>
2020-07-07 15:01:10 -0400

    First commit

---
+++ main.c
@@ -0,0 +1,12 @@
+/*
+Daniel Minami <minamid@sheridancollege.ca>
+----------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    exit(0);
+}
+
